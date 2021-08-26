cbc74323ce133103ef8a95f387cf52be6f405259
Annie Ausen <ausen.annie@gmail.com>
2021-08-26 16:41:12 -0500

    finish tetramino shapes

--- app.js
+++ app.js
@@ -14,5 +14,42 @@ const lTetramino = [
 ]
 
 const zTetramino = [
-    [width*3, width*2+1, width*3+1, width*2+2]
+    [width+1, width+2, width*2, width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0, width, width+1, width*2+1]
 ]
+
+const tTetramino = [
+    [1, width, width+1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
+]
+
+const oTetramino = [
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
+]
+
+const iTetramino = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+]
+
+const theTetraminos = [lTetramino, zTetramino, oTetramino, iTetramino]
+
+let currentPosition = 4;
+let current = theTetraminos[0][0];
+
+// draw the first rotaion in the first tetramino
+
+function draw () {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetramino')
+    })
+}
